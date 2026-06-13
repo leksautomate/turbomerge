25dd9232702d1905f50c5a9960e31ac5bd380c71
Leksi <woofiverr@gmail.com>
2026-06-13 12:58:34 +0100

    fix: add serverExternalPackages for CJS modules (archiver, pg-boss, mime-types)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ["archiver", "pg-boss", "mime-types", "@prisma/adapter-pg"],
 };
 
 export default nextConfig;
